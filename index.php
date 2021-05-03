c327ee9a8dcf989fa769135008c24e760afd6586
DicedBread <66288626+DicedBread@users.noreply.github.com>
2021-05-04 10:22:41 +1200

    int

---
+++ index.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <h1>Trident Marine</h1>
+            <!-- nav -->
+            <div class="nav">
+                <div>
+                    <h2>Home</h2>
+                </div>
+                <div>
+                    <h2>Courses</h2>
+                </div>
+                <div>
+                    <h2>About us</h2>
+                </div>
+            </div>
+        </div>
+        
+        <div class="main">
+
+        </div> 
+    </div>
+    
+</body>
+</html>
