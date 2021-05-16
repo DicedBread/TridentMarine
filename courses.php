7880d7f868a2901fb9553fa38b3ccd909264498d
DicedBread <66288626+DicedBread@users.noreply.github.com>
2021-05-17 10:24:12 +1200

    header included

---
+++ courses.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TridentMarine</title>
+    <link rel="stylesheet" href="style/font.css">
+    <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <?php include('includes/header.php') ?>
+    </div>
+    
+</body>
+</html>
