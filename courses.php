8e27876bf2f19ad8134fd92161f09e879424e492
DicedBread <66288626+DicedBread@users.noreply.github.com>
2021-05-21 09:46:57 +1200

    started courses nav

--- courses.php
+++ courses.php
@@ -8,14 +8,32 @@
     <link rel="stylesheet" href="style/footer.css">
     <link rel="stylesheet" href="style/courses.css">
     <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="wrapper">
         <?php include('includes/header.php') ?>
         <div class="courses_title_card">
-            <h1>Courses</h1>
+            <h2>COURSES</h2>
         </div>
         <div class="courses_nav">
+            <div class="courses_nav">
+                <div class="main_btn">
+                    <button>
+                        <h2>Power Boating</h2>
+                    </button>
+                    <button>
+                        <h2>Personal Watercraft</h2>
+                    </button>
+                    <button>
+                        <h2>Motor Cruising</h2>
+                    </button>
+                </div>
+                <div class="sub_btn">
+
+                </div>
+            </div>
             <!-- three main button  -->
             <!-- with second div with the display crap -->
         </div>
