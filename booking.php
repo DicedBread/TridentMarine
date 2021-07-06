5a74efcc3f7a771e8fbf04c1cd4593144adfd92a
DicedBread <66288626+DicedBread@users.noreply.github.com>
2021-07-06 13:08:01 +1200

    added something

--- booking.php
+++ booking.php
@@ -3,7 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TridentMarine</title>
-    <script src="jquery-3.6.0.min.js"></script>
+    <!-- <script src="jquery-3.6.0.min.js"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="script/courses_nav.js"></script>
     <link rel="stylesheet" href="style/font.css">
@@ -27,7 +27,9 @@
                     <input type="text" placeholder="Last Name...">
                     <br>
                     <input type="text" placeholder="Email...">
-                    
+
+                    <h3>Selected course</h3>
+                        <p>Level 1 Start Powerboating</p>
 
                 </fieldset>
             </form>
